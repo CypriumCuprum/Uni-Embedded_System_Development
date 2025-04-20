bac5468e4231816fa7f5ee8109a2120f9c9a463f
BunRealCua123 <hieukv26102003@gmail.com>
2025-04-20 20:48:13 +0700

    update

--- front_end/src/components/TrafficLight.jsx
+++ front_end/src/components/TrafficLight.jsx
@@ -1,52 +1,53 @@
 import { useEffect, useState } from 'react';
 import './TrafficDashboard.css';
-const TrafficLight = ({ position, cycle, updateCycle }) => {
-    const [lightState, setLightState] = useState(position === 'left' ? 'green' : 'red');
-    const [timer, setTimer] = useState(position === 'left' ? parseInt(cycle) : parseInt(cycle) + 5); // Start with green for left and red for right
+const TrafficLight = ({ light }) => {
+    console.log('light', light);
+    // const [lightState, setLightState] = useState(position === 'left' ? 'green' : 'red');
+    // const [timer, setTimer] = useState(position === 'left' ? parseInt(cycle) : parseInt(cycle) + 5); // Start with green for left and red for right
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setTimer((prevTimer) => {
-                if (prevTimer <= 1) {
-                    // Change light state when timer reaches 0
-                    if (lightState === 'red') {
-                        setLightState('green');
-                        return parseInt(cycle); // Green light duration
-                    } else if (lightState === 'green') {
-                        setLightState('yellow');
-                        return 5; // Yellow light duration
-                    } else {
-                        setLightState('red');
-                        return parseInt(cycle) + 5; // Red light duration
-                    }
-                }
-                return prevTimer - 1;
-            });
-        }, 1000);
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         setTimer((prevTimer) => {
+    //             if (prevTimer <= 1) {
+    //                 // Change light state when timer reaches 0
+    //                 if (lightState === 'red') {
+    //                     setLightState('green');
+    //                     return parseInt(cycle); // Green light duration
+    //                 } else if (lightState === 'green') {
+    //                     setLightState('yellow');
+    //                     return 5; // Yellow light duration
+    //                 } else {
+    //                     setLightState('red');
+    //                     return parseInt(cycle) + 5; // Red light duration
+    //                 }
+    //             }
+    //             return prevTimer - 1;
+    //         });
+    //     }, 1000);
 
-        return () => clearInterval(interval);
-    }, [lightState, cycle]);
+    //     return () => clearInterval(interval);
+    // }, [lightState, cycle]);
 
-    if (position === 'left') {
-        console.log('cycle', cycle);
-        console.log('tỉmer', timer);
-    }
+    // if (position === 'left') {
+    //     console.log('cycle', cycle);
+    //     console.log('tỉmer', timer);
+    // }
 
-    useEffect(() => {
-        if (lightState === 'yellow' && updateCycle) {
-            updateCycle();
-        }
-    }, [lightState]);
+    // useEffect(() => {
+    //     if (lightState === 'yellow' && updateCycle) {
+    //         updateCycle();
+    //     }
+    // }, [lightState]);
     // console.log('position ' + position + ';lightState ' + lightState + ';timer ', timer);
 
     return (
         <div className="traffic-light">
             <div className="light-container">
-                <div className={`light red ${lightState === 'red' ? 'active' : ''}`}></div>
-                <div className={`light yellow ${lightState === 'yellow' ? 'active' : ''}`}></div>
-                <div className={`light green ${lightState === 'green' ? 'active' : ''}`}></div>
+                <div className={`light red ${light?.color === 'RED' ? 'active' : ''}`}></div>
+                <div className={`light yellow ${light?.color === 'YELLOW' ? 'active' : ''}`}></div>
+                <div className={`light green ${light?.color === 'GREEN' ? 'active' : ''}`}></div>
             </div>
-            <div className="timer">{timer}s</div>
+            <div className="timer">{light?.content}s</div>
         </div>
     );
 };
